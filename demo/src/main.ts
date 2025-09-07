2efd89a1ec62307297c4a1bcc77daaeedd7c1d69
Jānis Erlihs <erlihs@gmail.com>
2025-09-07 06:46:07 +0300

    remove router

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -3,14 +3,11 @@ import { createPinia } from 'pinia'
 import { createPiniaPluginStorage } from './plugin'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 const pinia = createPinia()
 pinia.use(createPiniaPluginStorage)
 app.use(pinia)
-
-app.use(router)
 
 app.mount('#app')
