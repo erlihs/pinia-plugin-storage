4685a431b571253a3c75075c45c4b3cd8b618be5
Jānis Erlihs <erlihs@gmail.com>
2025-09-07 08:51:44 +0300

    demo - none

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,22 +1,59 @@
 <script setup lang="ts">
+import { useCounterStoreNone } from './stores/counter-none'
 import {
-  useCounterStore,
   useCounterStoreDefault,
   useCounterStoreAdvanced,
   useCounterStoreSession,
   useCounterStoreIndexedDB,
   useCounterStoreCrossTab,
 } from './stores/counter'
-const counterStore = useCounterStore()
+
+const counterStoreNone = useCounterStoreNone()
 const counterStoreDefault = useCounterStoreDefault()
 const counterStoreAdvanced = useCounterStoreAdvanced()
 const counterStoreSession = useCounterStoreSession()
 const counterStoreIndexedDB = useCounterStoreIndexedDB()
 const counterStoreCrossTab = useCounterStoreCrossTab()
+
+const reloadPage = () => {
+  window.location.reload()
+}
 </script>
 
 <template>
   <h1>Pinia Plugin Storage</h1>
+
+  <h2>None</h2>
+
+  <table>
+    <thead>
+      <tr>
+        <th>storageType</th>
+        <th>Action</th>
+        <th>Value</th>
+        <th>Expected behavior</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>None</td>
+        <td>
+          <button @click="counterStoreNone.increment(-1)">-</button>
+          <button @click="counterStoreNone.increment(1)">+</button>
+        </td>
+        <td>{{ counterStoreNone.count }}&nbsp;&nbsp;{{ counterStoreNone.extCount }}</td>
+        <td>Values should not persist after page reload</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="4" style="text-align: right">
+          <button @click="reloadPage">🔄 Reload Page</button>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+
   <h2>Source</h2>
   <p>//todo</p>
   <h2>Tests</h2>
@@ -32,13 +69,6 @@ const counterStoreCrossTab = useCounterStoreCrossTab()
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>none</td>
-        <td><button @click="counterStore.increment()">+</button></td>
-        <td>{{ counterStore.count }}</td>
-        <td>{{ counterStore.extCount }}</td>
-        <td>Values should not persist after page reload</td>
-      </tr>
       <tr>
         <td>localStorage</td>
         <td><button @click="counterStoreDefault.increment()">+</button></td>
@@ -112,4 +142,55 @@ const counterStoreCrossTab = useCounterStoreCrossTab()
   </table>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Minimal normalize */
+* {
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 0.85rem;
+}
+
+/* Headings */
+h1 {
+  font-size: 1.3rem;
+  margin: 1.5rem 0 1rem 0;
+}
+
+h2 {
+  font-size: 1.15rem;
+  margin: 1.25rem 0 0.75rem 0;
+}
+
+h3 {
+  font-size: 1rem;
+  margin: 1rem 0 0.5rem 0;
+}
+
+/* Buttons */
+button {
+  cursor: pointer;
+  padding: 6px 12px;
+  margin-right: 3px;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  background-color: #f9f9f9;
+  transition: all 0.2s ease;
+}
+
+button:hover {
+  background-color: #e9e9e9;
+  border-color: #999;
+}
+
+/* Tables */
+table {
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+</style>
---
+++ demo/src/stores/counter-none.ts
@@ -0,0 +1,20 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStoreNone = defineStore('counter-none', () => {
+  const count = ref(0)
+
+  const extCount = ref({
+    decimal: 0,
+    hex: '0x0',
+  })
+
+  function increment(incrementBy?: number) {
+    count.value += incrementBy ?? 1
+
+    extCount.value.decimal = count.value
+    extCount.value.hex = '0x' + count.value.toString(16)
+  }
+
+  return { count, extCount, increment }
+})
